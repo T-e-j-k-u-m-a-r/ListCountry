9bb4922be6b3594d7bf781844cda7901e85d0cd1
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-01-20 15:54:06 +0530

    Added the country phone and languages

--- src/listCountryCodes.js
+++ src/listCountryCodes.js
@@ -4,5 +4,6 @@ const { countries } = require('countries-list');
 // Output: { ... 'CO': 'Congo', ... }
 console.log(`Country name is ${countries.ID.name}`)
 console.log(`Country capital is ${countries.ID.capital}`)
-console.log(`Country currency is ${countries.ID.phone}`)
+console.log(`Country phone is ${countries.ID.phone}`)
+console.log(`Country languages is ${countries.ID.languages}`)
 
