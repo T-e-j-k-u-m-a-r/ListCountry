d563052a6463a9c5e98b9c50745532184624a239
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-01-21 10:58:03 +0530

    Handled merge conflicts

--- src/listCountryCodes.js
+++ src/listCountryCodes.js
@@ -6,4 +6,9 @@ console.log(`Country name is ${countries.ID.name}`)
 console.log(`Country capital is ${countries.ID.capital}`)
 console.log(`Country phone is ${countries.ID.phone}`)
 console.log(`Country languages is ${countries.ID.languages}`)
+console.log(`Country continent = ${countries.IL.continent}`)
+//console.log(`Country emojiU = ${countries.IN.emojiU}`)
+
+
+
 
