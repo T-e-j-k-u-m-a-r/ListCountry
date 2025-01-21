a12d136836043e7d253f553f83e2683edef651f5
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-01-21 15:47:29 +0530

    Updated the AD currency to the list

--- src/listCountryCodes.js
+++ src/listCountryCodes.js
@@ -1,14 +1,12 @@
 // Import the countries-list package
-const { countries } = require('countries-list');
+const { continents  } = require('countries-list');
 
-// Output: { ... 'CO': 'Congo', ... }
-console.log(`Country name is ${countries.ID.name}`)
+console.log(`Country name is ${languages.af.name}`)
 console.log(`Country capital is ${countries.ID.capital}`)
 console.log(`Country phone is ${countries.ID.phone}`)
 console.log(`Country languages is ${countries.ID.languages}`)
 console.log(`Country continent = ${countries.IL.continent}`)
-//console.log(`Country emojiU = ${countries.IN.emojiU}`)
-
-
+console.log(`Country emojiU = ${countries.IN.emojiU}`)
+console.log(`countries.AD.currency = ${countries.AD.currency}`)
 
 
