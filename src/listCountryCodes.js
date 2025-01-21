f4ff8aef9cc931fd0852caac713c279888037418
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-01-21 10:50:20 +0530

    Revert "Commited emoji line"
    This reverts commit 6c90c60273725f58bb591879941478aad665e48d.

--- src/listCountryCodes.js
+++ src/listCountryCodes.js
@@ -7,7 +7,7 @@ console.log(`Country capital is ${countries.ID.capital}`)
 console.log(`Country phone is ${countries.ID.phone}`)
 console.log(`Country languages is ${countries.ID.languages}`)
 console.log(`Country continent = ${countries.IL.continent}`)
-//console.log(`Country emojiU = ${countries.IN.emojiU}`)
+console.log(`Country emojiU = ${countries.IN.emojiU}`)
 
 
 
